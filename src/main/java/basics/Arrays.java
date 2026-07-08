a5d1609b781ef8d9a887e5727b179da108a616d5
Alba Carrasco <albacarrasmel@gmail.com>
2026-07-08 10:51:10 +0200

    #1 Replaced the use of System.out by a logger. because SonarQube detected this usage as a bad-practice

--- src/main/java/basics/Arrays.java
+++ src/main/java/basics/Arrays.java
@@ -1,5 +1,8 @@
 package basics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /*
 
  * Technical demonstration of basic array operations in Java:
@@ -11,41 +14,43 @@ package basics;
 
 public class Arrays {
 
+    private static final Logger logger = LoggerFactory.getLogger(Arrays.class);
+
 	public static void main() {
 
         // Unidimensional array of integers
         int[] num = {5, 3, 8, 1, 9};
-        System.out.println("--- Array unidimensional ---");
-        System.out.print("Elements: ");
+        logger.info("--- Array unidimensional ---");
+        logger.info("Elements: ");
         for (int n : num) {
-            System.out.print(n + " ");
+            logger.info("{} ", n);
         }
-        System.out.println();
-        System.out.println("Length: " + num.length);
-        System.out.println("First element: " + num[0]);
-        System.out.println("Last element: " + num[num.length - 1]);
+        logger.info("");
+        logger.info("Length: {}", num.length);
+        logger.info("First element: {}", num[0]);
+        logger.info("Last element: {}", num[num.length - 1]);
 
         // Unidimensional array of strings
         String[] names = new String[3];
         names[0] = "name0";
         names[1] = "name1";
         names[2] = "name2";
-        System.out.println("Names: " + java.util.Arrays.toString(names));
+        logger.info("Names: {}", java.util.Arrays.toString(names));
 
         // Finding the maximum value in the integer array
         int max = num[0];
         for (int n : num) {
             if (n > max) max = n;
         }
-        System.out.println("Maximum value: " + max);
+        logger.info("Maximum value: {}", max);
 
         // Cloning and sorting the integer array
         int[] copy = num.clone();
         java.util.Arrays.sort(copy);
-        System.out.println("Sorted: " + java.util.Arrays.toString(copy));
+        logger.info("Sorted: {}", java.util.Arrays.toString(copy));
 
         // 2D array of integers
-        System.out.println("--- Matrix 2D ---");
+        logger.info("--- Matrix 2D ---");
         int[][] matrix = {
             {1, 2, 3},
             {4, 5, 6},
@@ -53,9 +58,9 @@ public class Arrays {
         };
         for (int line = 0; line < matrix.length; line++) {
             for (int col = 0; col < matrix[line].length; col++) {
-                System.out.print(matrix[line][col] + " ");
+                logger.info("{} ", matrix[line][col]);
             }
-            System.out.println();
+            logger.info("");
         }
     }
 }
