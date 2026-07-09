0acab80d2505a053cb334f6531b2abb900d46821
Alba Carrasco <albacarrasmel@gmail.com>
2026-07-09 10:13:49 +0200

    Added the file Classes with a simple example of a person with just name and age, that includes a method to introduce himself and a method that increases the age. I will update more this file because I think is very basic, and we can put more things into the class or even create another one like a 'house' for example

---
+++ src/main/java/oop/Classes.java
@@ -0,0 +1,51 @@
+package oop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+
+ * Classes and objects: attributes, constructor, methods, this.
+
+*/
+
+class Person {
+
+    private static final Logger logger = LoggerFactory.getLogger(Person.class);
+
+    String name;
+    int age;
+
+    // Constructor to initialize the name and age attributes
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to introduce the person which prints their name and age
+    void introduceOneself() {
+        logger.info("Hello, I am {} and I am {} years old.", name, age);
+    }
+
+    // Method to celebrate a birthday which increases the age
+    void haveBirthday() {
+        age++;
+    }
+}
+
+public class Classes {
+
+    public static void main(){
+
+        final Logger logger = LoggerFactory.getLogger(Classes.class);
+
+        Person p1 = new Person("exampleName1", 28);
+        Person p2 = new Person("exampleName2", 34);
+
+        p1.introduceOneself();
+        p2.introduceOneself();
+
+        p1.haveBirthday();
+        logger.info("{} is now {} years old.", p1.name, p1.age);
+    }
+}
