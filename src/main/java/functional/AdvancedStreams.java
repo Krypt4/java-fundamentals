c9ed08c039926af1b7d6eff65071a8872613ee4f
Alba Carrasco <albacarrasmel@gmail.com>
2026-07-14 00:56:21 +0200

    #22 Added basic description of the class

--- src/main/java/functional/AdvancedStreams.java
+++ src/main/java/functional/AdvancedStreams.java
@@ -2,5 +2,11 @@ package functional;
 
 // https://blog.tangly.net/blog/2024/advanced-streams/
 
+/*
+
+ * Advanced flows: Reduce, groupBy, sorted with comparator, statistics
+
+*/
+
 public class AdvancedStreams {
 }
