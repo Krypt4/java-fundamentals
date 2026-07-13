6a399964d3c0af589744e6e5093ee94ecc868a69
Alba Carrasco <albacarrasmel@gmail.com>
2026-07-14 00:56:01 +0200

    #20 Added basic description of the class

--- src/main/java/functional/LambdasBasics.java
+++ src/main/java/functional/LambdasBasics.java
@@ -2,5 +2,11 @@ package functional;
 
 // https://www.geeksforgeeks.org/java/lambda-expressions-java-8/
 
+/*
+
+ * Lambda expressions and functional interfaces
+
+*/
+
 public class LambdasBasics {
 }
