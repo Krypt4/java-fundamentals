30cccba7036ba2fd4b96723d73fa4d555a0c73b0
Alba Carrasco <albacarrasmel@gmail.com>
2026-07-14 00:56:10 +0200

    #21 Added basic description of the class

--- src/main/java/functional/StreamsBasics.java
+++ src/main/java/functional/StreamsBasics.java
@@ -2,5 +2,11 @@ package functional;
 
 // https://www.hungrycoders.com/blog/understanding-java-streams-with-code-examples
 
+/*
+
+ * Streams: Filter, map, forEach, collect (basic operations on collections)
+
+*/
+
 public class StreamsBasics {
 }
